bed1e72fd45d8e630c348cafb056929443f5ae75
Evgeny Gunko <evgeny.gunko@gmail.com>
2018-10-15 21:02:37 +0000

    Merged PR 95: Update dependencies

---
+++ Build/download-latest-release.ps1
@@ -0,0 +1,21 @@
+# Download latest release artifacts from github
+$repo = "evgenygunko/CopyWords.Parsers"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+$file = "CopyWords.Parsers.Evgeny.$tag.nupkg"
+
+$downloadUrl = "https://github.com/$repo/releases/download/$tag/$file"
+Write-Host "downloadUrl = $downloadUrl"
+
+$targetFolder = "$env:BUILD_SOURCESDIRECTORY/.nuget/local_nuget"
+#$targetFolder = "/Temp/packages"
+$targetFile = "$targetFolder/$file"
+Write-Host "targetFile = $targetFile"
+
+New-Item -ItemType Directory -Force -Path $targetFolder
+
+Write-Host Dowloading latest release
+Invoke-WebRequest $downloadUrl -Out $targetFile
---
+++ CopyWordsWPF/ViewModel/Commands/PlaySoundCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CopyWordsWPF.ViewModel.Commands
+{
+    public class PlaySoundCommand : CommandBase
+    {
+        public override void Execute(object parameter)
+        {
+            string soundURL = parameter as string;
+
+            if (!soundURL.EndsWith(".mp3"))
+            {
+                throw new ArgumentException("Sound file URL doesn't end with '.mp3'");
+            }
+
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+            using (WebClient wc = new WebClient())
+            {
+                using (Stream fileStream = wc.OpenRead(soundURL))
+                {
+                    player.Load(fileStream);
+                    player.Play();
+                }
+            }
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
